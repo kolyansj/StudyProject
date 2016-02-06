0209ae2dceadb168c791e28f83560f9494305ee7
kolyansj <kolyansj@gmail.com>
2016-02-07 01:09:28 +0400

    add faculty object

---
+++ src/com/example/solution/model/Faculty.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.solution.model;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Nikolay
+ */
+@XmlRootElement(name = "faculty")
+public class Faculty implements Serializable {
+    
+    private String id;
+    private String name;
+    private String abbreviation;
+    private List<Group> groups;
+    
+    public Faculty() {
+    }
+
+    public Faculty(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }       
+
+    @XmlAttribute(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @XmlAttribute(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @XmlAttribute(name = "abbreviation")
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    @XmlElementWrapper(name="groups")
+    @XmlElement(name = "group")
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 13 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Faculty other = (Faculty) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+}
